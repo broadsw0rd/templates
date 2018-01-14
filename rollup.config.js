f7bc6f6a63614d9b0998ba5e2a6df6645074c968
Vladimir Bykov <broadsw.rd@yandex.ru>
2018-01-14 21:31:43 +0300

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import pkg from './package.json'
+import buble from 'rollup-plugin-buble'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+
+export default {
+  input: 'src/index.js',
+  output: [
+    { file: pkg.umd, format: 'umd', name: 'Templates' },
+    { file: pkg.main, format: 'cjs' },
+    { file: pkg.module, format: 'es' }
+  ],
+  plugins: [
+    buble(),
+    nodeResolve(),
+    commonjs()
+  ]
+}
